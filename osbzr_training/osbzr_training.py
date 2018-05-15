2408a90c3b27d933e3bda7b6e0c96c8da1652003
Roggie <fire8862005@gmail.com>
2018-05-15 17:48:37 +0800

    开发培训第二天-丁伟

--- osbzr_training/osbzr_training.py
+++ osbzr_training/osbzr_training.py
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
-from  odoo  import  models,fields
+from  odoo  import  models,fields,api,exceptions
 class training_subject(models.Model):
     _name = 'training.subject'
     _rec_name = 'name'
@@ -15,6 +15,18 @@ class training_subject(models.Model):
 
     description = fields.Text('描述')
 
+    @api.multi
+    def name_get(self):
+        res = super(training_subject,self).name_get()
+        result =[]
+        if self.env.context.get("form"):
+            for index, name in res:
+                subject = self.browse(index)
+                result.append((index,name + "-%s" % subject.manager_id.name or ""))
+            return result
+        else:
+            return res
+
 class training_lesson(models.Model):
     _name = 'training.lesson'
     _rec_name = 'name'
@@ -29,10 +41,39 @@ class training_lesson(models.Model):
     start_date = fields.Date("开始日期")
     end_date = fields.Date("结束日期")
     sites = fields.Integer("座位数")
+    state = fields.Selection([("new",u'招生'),("start",u'已开课'),("end",u'结束')],string ="状态",default ="new")
     teacher_id = fields.Many2one(comodel_name='res.partner',string="老师",domain="[('is_teacher','=',True)]")
     student_ids = fields.Many2many(comodel_name='res.partner', string="学生")
+    _sql_constraints = [
+        ('uniq_name','unique(name)','课程名必须唯一'),
+    ]
+    @api.constrains('end_date','start_date')
+    def check_date(self):
+        for lesson in self :
+            if lesson.end_date <lesson.start_date :
+                raise exceptions.ValidationError(u'开始日期不能大于结束日期')
+
+    @api.constrains('sites')
+    def check_lesson(self):
+        for lesson in self :
+            if lesson.sites <0 :
+                raise exceptions.ValidationError(u'坐席数不能为零')
+
+    @api.multi
+    def start(self):
+        for lesson in self :
+            self.state ="start"
+
+    @api.multi
+    def end(self):
+        for lesson in self :
+            self.state ="end"
 
 class res_partner(models.Model):
     _inherit = 'res.partner'
 
-    is_teacher=fields.Boolean(u'是老师')
+    @api.model
+    def _is_teacher(self):
+        return self.env.context.get('teacher')
+
+    is_teacher=fields.Boolean(u'是老师',default =_is_teacher)
